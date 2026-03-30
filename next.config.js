d031693a8670ff8173f51d112d48aa326e005d57
anna9-hc <anna9-hc@users.noreply.github.com>
2026-03-30 17:55:23 +0800

    feat: add Cloudflare Pages support

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   env: {
     REMOVEBG_API_KEY: process.env.REMOVEBG_API_KEY,
   },
+  experimental: {
+    allowedDevOrigins: ['http://43.159.142.113:3000'],
+  },
 }
 
 module.exports = nextConfig
